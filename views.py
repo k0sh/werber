1a5a71d7c9132090abdf7ea00effee798002a1a8
Renat <renatn@gmail.com>
2009-10-19 22:49:52 +0400

    Added simple template and view function

--- views.py
+++ views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from django.template import loader, Context
+from django.http import HttpResponse
+from mysite.werber.models import SquidUser
+
+def squidusers(request):
+	users = SquidUser.objects.all()
+	t = loader.get_template("main.html")
+	c = Context({'users' : users})
+	return HttpResponse(t.render(c))
+
