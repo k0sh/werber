3db8ee0153e5c0e20eb5172a93b46af4458a9a7a
Renat <renatn@gmail.com>
2009-10-23 23:57:06 +0400

    Добавил код который парсит access.log и суммирует трафик по ip

--- daemon/werberd.py
+++ daemon/werberd.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python
 # -*- mode: python; coding: utf-8; -*-
 
+import sys
+
+db = {}
+
+def ip_update(ip, size):
+    if db.has_key(ip):
+        db[ip] += size
+    else:
+        db[ip] = size
+
+for line in sys.stdin:
+    raw_list = line.split()
+    ip_update(raw_list[2], int(raw_list[4]))
+
+print db
+
